150cc8116ebbf6ea8b42e8dae2752c009b3e0273
YM <ymacedo05@gmail.com>
2022-09-30 03:01:16 -0300

    ...
    ...

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Menu Mobile
 let btn = document.getElementById("btn")
 let nav = document.getElementById("nav")
 
@@ -21,4 +23,30 @@ console.log(span, resumo)
 function mouse(y){
     resumo[y].classList.toggle("active")
     span[y].classList.toggle("active")
+}
+
+// Componente Login
+let logar = document.getElementById("logar")
+logar.addEventListener("click", (x) => addComponente(x))
+
+
+
+function addComponente(x){
+    console.log(x)
+    login.classList.add("active")
+
+}
+
+let login = document.getElementById("login")
+login.addEventListener("click", (y) => removeComponente(y))
+
+let btnX = document.getElementById("btn-x")
+btnX.addEventListener("click", (y) => removeComponente(y))
+
+function removeComponente(y){
+    
+    if(y.target.id == "btn-x" || y.target.id == "login" ) {
+        login.classList.remove("active")
+    }
+    
 }
